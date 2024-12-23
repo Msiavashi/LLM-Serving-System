cf4ab15fab185168553e17d28420acd4cd31bcf4
mohammad <mohammad.siavashi@outlook.com>
2024-12-23 19:20:26 +0100

    Add experimental timeout threshold

--- src/queues/fcfs_queue.py
+++ src/queues/fcfs_queue.py
@@ -1,17 +1,19 @@
 from src.queues.base_queue import BaseQueue
+import time
 
 class FCFSQueue(BaseQueue):
     """First-Come, First-Served (FCFS) queue implementation."""
     
     def enqueue(self, item):
-        self.queue.append(item)
+        self.queue.append((item, time.time()))
     
     def dequeue(self):
         if self.is_empty():
             raise IndexError("Dequeue from an empty queue.")
-        return self.queue.pop(0)
+        return self.queue.pop(0)[0]
 
     def peek(self):
+        """Return the item at the front of the queue without removing it as tuple (object, insertion_timestamp)."""
         if self.is_empty():
             raise IndexError("Peek from an empty queue.")
         return self.queue[0]
