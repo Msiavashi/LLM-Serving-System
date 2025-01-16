923d58cbb72d4437639c31ad83cd7694b87268f5
mohammad <mohammad.siavashi@outlook.com>
2025-01-16 13:54:49 +0100

    Add experimental online inference with async scheduler and redis queue To use the online inference refer to README.md. Briefly: run redis, run scheduler service, then, run api_server.py, finally you can send requests as provided in async example.

--- src/cache/unified_dynamic_cache.py
+++ src/cache/unified_dynamic_cache.py
@@ -12,6 +12,9 @@ class UnifiedDynamicCache(DynamicCache):
     def split_kv_cache(self):
         return self.caches
     
+    def _update_single_cache(self, cache, key_state, value_state, layer_idx, cache_kwargs):
+        return cache.update(key_state, value_state, layer_idx, cache_kwargs)
+
     def update(
             self,
             key_states: torch.Tensor,
@@ -19,6 +22,7 @@ class UnifiedDynamicCache(DynamicCache):
             layer_idx: int,
             cache_kwargs: Optional[Dict[str, Any]] = None,
         ) -> Tuple[torch.Tensor, torch.Tensor]:
+            
             key_list = []
             value_list = []
             max_len = 0
@@ -28,12 +32,17 @@ class UnifiedDynamicCache(DynamicCache):
                 key_list.append(keys)
                 value_list.append(values)
                 max_len = max(max_len, keys.shape[1])
-            self.max_len = max_len
-            padded_key_list = [torch.nn.functional.pad(tensor, (0, 0, 0, max_len - tensor.shape[1])) for tensor in key_list]
-            padded_value_list = [torch.nn.functional.pad(tensor, (0, 0, 0, max_len - tensor.shape[1])) for tensor in value_list]
             
-            merged_key_states = torch.stack(padded_key_list)
-            merged_value_states = torch.stack(padded_value_list)
+            self.max_len = max_len
+            
+            for i in range(len(key_list)):
+                pad_length = max_len - key_list[i].shape[1]
+                if pad_length > 0:
+                    key_list[i] = torch.nn.functional.pad(key_list[i], (0, 0, 0, pad_length))
+                    value_list[i] = torch.nn.functional.pad(value_list[i], (0, 0, 0, pad_length))
+            
+            merged_key_states = torch.stack(key_list, dim=0)
+            merged_value_states = torch.stack(value_list, dim=0)
             
             return merged_key_states, merged_value_states
 
