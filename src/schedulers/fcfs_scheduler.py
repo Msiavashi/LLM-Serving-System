094e4bf72191e332feed9f04cbc1267dd1642bd2
mohammad <mohammad.siavashi@outlook.com>
2025-02-14 23:38:24 +0100

    Refactor FCFS scheduler

--- src/schedulers/fcfs_scheduler.py
+++ src/schedulers/fcfs_scheduler.py
@@ -6,20 +6,4 @@ class FCFSScheduler(BaseFCFSScheduler):
         super().__init__(engine, tokenizer, batch_size)
 
     def run_scheduler(self):
-        finished_sequences = []
-        
-        while not (self.decode_queue.is_empty() and self.prefill_queue.is_empty()):
-             
-            batch, is_decode = self._get_next_batch()
-             
-            if batch.size() == 0:
-                break
-            
-            start_time = time.time()
-            output_batch = self.engine.run_batch(batch)
-            elapsed = time.time() - start_time
-            
-            finished_sequences += self._post_process_batch(output_batch, elapsed, is_decode)
-             
-        self.monitor.print_final_stats()
-        return finished_sequences
+        return self.run_loop(lambda batch: self.engine.run_batch(batch))
